3b81ca14687dbeeb84fa05d53c6ebc1865848cb7
bilalbbh106-hash <bilalbbh106@gmail.com>
2025-12-14 21:18:00 +0100

    Create ads.js

---
+++ js/ads.js
@@ -0,0 +1,7 @@
+const ADS_PROVIDER = "adsterra"; // none لإيقاف الإعلانات
+
+if (ADS_PROVIDER === "adsterra") {
+  const s = document.createElement("script");
+  s.src = "https://fedreferawake.com/d7/46/2b/d7462bcbda4583736b172b65f4093349.js";
+  document.body.appendChild(s);
+}
